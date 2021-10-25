eecbd909941a3704614ed732ba25cba18552c258
p-nikitsenka <p.nikitsenka@gmail.com>
2021-10-26 00:25:36 +0300

    initial

---
+++ src/main/java/by/acdemy/classwork/lesson1/Main.java
@@ -0,0 +1,14 @@
+package by.acdemy.classwork.lesson1;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] x = {3,4,5,6};
+        String str;
+        str = Arrays.toString(x);
+        System.out.println(str);
+    }
+}
+
+
