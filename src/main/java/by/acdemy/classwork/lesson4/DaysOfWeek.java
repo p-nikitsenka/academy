bbf10923e1d33dc9719124cea78691af783210f9
p-nikitsenka <p.nikitsenka@gmail.com>
2021-10-30 23:33:19 +0300

    lesson 4 added

---
+++ src/main/java/by/acdemy/classwork/lesson4/DaysOfWeek.java
@@ -0,0 +1,8 @@
+package by.acdemy.classwork.lesson4;
+
+public class DaysOfWeek {
+    public static void main(String[] args) {
+        String[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+        System.out.println(days[6]);
+    }
+}
---
+++ src/main/java/by/acdemy/classwork/lesson4/MassiveDouble.java
@@ -0,0 +1,12 @@
+package by.acdemy.classwork.lesson4;
+
+public class MassiveDouble {
+    public static void main(String[] args) {
+        double[] array = new double[4];
+        array[0] = 2.134;
+        array[1] = 7.179;
+        array[2] = 9.214;
+        array[3] = 0.214;
+        System.out.println(array[2]);
+    }
+}
